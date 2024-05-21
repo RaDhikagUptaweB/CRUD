6ea9bdd1e34d69f3ca887f6fa477cec9b682c9cd
Radhika gupta <67688493+RaDhikagUptaweB@users.noreply.github.com>
2024-05-21 22:29:50 +0530

    Add files via upload

---
+++ DAOService.java
@@ -0,0 +1,15 @@
+package com.ims.model;
+
+import java.sql.ResultSet;
+
+public interface DAOService {
+public void connectDB();
+public boolean verfyLogin(String email,String password);
+public ResultSet getAllInquiries();
+public void addInquiry(String name,String email,String mobile,String course);
+public void deleteInquiry(String email);
+public void updateInquiry(String email, String mobile);
+public void closeDB();
+
+
+}
